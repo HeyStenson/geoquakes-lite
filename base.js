7ff46fa2aeb7d65ace5d49dc55b70101ccb909f1
HeyStenson <h.n.stenson@gmail.com>
2016-03-03 11:25:35 -0800

    basic functionality

--- base.js
+++ base.js
@@ -1,21 +1,28 @@
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 var map;
 
 $(document).ready(function(){
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 37.6189, lng: -122.3750},
+    zoom: 3
   });
 
-})
+  $.get(weekly_quakes_endpoint, function(data){
+  		var quakes = data.features;
+  		quakes.forEach(function(quake){
+  			var title = quake.properties.title;
+  			var hours_ago = Math.round( ( Date.now() - quake.properties.time ) / (1000*60*60) );
+  			$('#info').append('<h2>' + title + '</h2>' + '<p>This quake happened ' + hours_ago + ' hours ago' + '</p>' + '<hr>');
+  			var lat = quake.geometry.coordinates[1];
+  			var lng = quake.geometry.coordinates[0];
+  			new google.maps.Marker({
+  			  position: new google.maps.LatLng(lat,lng),
+  			  map: map,
+  			  title: title
+  			});
+  		});
+  });
 
-// This is a code snippet that will place a pin at the coordinates lat & lng
-/*
-new google.maps.Marker({
-  position: new google.maps.LatLng(lat,lng),
-  map: map,
-  title: title
 });
-*/
